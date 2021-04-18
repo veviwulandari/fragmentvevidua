96a59009dfd5f8e0e1611d24b05b2f1b30d309e2
veviwulandari <veviwulandari20@gmail.comgit config --global user.email veviwulandari20@gmail.com>
2021-04-18 22:33:12 +0700

    fragmentvevidua

---
+++ app/src/main/java/id/ac/pelitabangsa/fragmentvevi/SecondActivity.java
@@ -0,0 +1,28 @@
+package id.ac.pelitabangsa.fragmentvevi;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Log.v("CREATING", "SECOND ACTIVITY");
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            finish();
+            return;
+        }
+
+        if (savedInstanceState == null){
+            SecondFragment details = new SecondFragment();
+            getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+        }
+    }
+}
